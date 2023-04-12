240f1eea0911636aa11e7b826e8820a8ca012542
YZahorodnii <yzagorodniy@gmail.com>
2023-04-12 14:45:26 +0100

    homework to lesson 1 complete

---
+++ src/components/character/Character.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import character from "./Character";
+
+const Character = ({character}) => {
+    const {id,name,status,species,gender,image} = character;
+    return (
+        <div>
+            <div>id:{id}</div>
+            <div>name:{name}</div>
+            <div>status:{status}</div>
+            <div>species:{species}</div>
+            <div>gender:{gender}</div>
+            <img src={image} alt={name}/>
+        </div>
+    );
+};
+
+export default Character;
