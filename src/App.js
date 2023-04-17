da3e0fce658cb11a02cf4a8acc180647123328b1
YZahorodnii <yzagorodniy@gmail.com>
2023-04-17 12:47:19 +0100

    homework to lesson 2

--- src/App.js
+++ src/App.js
@@ -1,13 +1,11 @@
-import Simpsons from "./components/Simpsons/Simpsons";
-import Characters from "./components/characters/Characters";
+
+
 
 function App() {
+
   return (
     <div className="App">
-      {<Simpsons/>}
-        {
-            <Characters/>
-        }
+
     </div>
   );
 }
