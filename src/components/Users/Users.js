1d18604b79455364d358a7e3d968ab4dcd4b8494
YZahorodnii <yzagorodniy@gmail.com>
2023-04-19 12:21:44 +0100

    homework to lesson 3 with Users and Comments

---
+++ src/components/Users/Users.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import User from "../User/User";
+
+const Users = () => {
+    return (
+        <div>
+            <User/>
+        </div>
+    );
+};
+
+export default Users;
