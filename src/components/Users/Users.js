21d8290c8ae83f523443e153bfc6026c4a8b70f4
YZahorodnii <yzagorodniy@gmail.com>
2023-04-19 12:25:55 +0100

    homework to lesson 3 with Users and Comments

--- src/components/Users/Users.js
+++ src/components/Users/Users.js
@@ -1,3 +1,8 @@
+// Зробити компонент, в якому буде форма, за допомоги якої можливо створити
+// нового юзера постовим запитом на http://jsonplaceholder.typicode.com/users
+// Зробити компонент, в якому буде форма, за допомоги якої можливо створити
+// новий комментар постовим запитом на http://jsonplaceholder.typicode.com/comments
+
 import React from 'react';
 import User from "../User/User";
 
