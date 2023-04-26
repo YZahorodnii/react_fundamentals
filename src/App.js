740d71094c49e1b7629eef400c12ee17685d58ee
YZahorodnii <yzagorodniy@gmail.com>
2023-04-26 10:48:08 +0100

    code refactoring for homework #3 Form

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Users from "./Components/Users/Users";
+import Comments from "./Components/Comments/Comments";
+
+const App = () => {
+    return (
+        <div>
+            <Users/>
+             <Comments/>
+        </div>
+    );
+};
+
+export default App;
--- src/components/Users/Users.js
+++ src/components/Users/Users.js
@@ -2,25 +2,22 @@
 // нового юзера постовим запитом на http://jsonplaceholder.typicode.com/users
 // Зробити компонент, в якому буде форма, за допомоги якої можливо створити
 // новий комментар постовим запитом на http://jsonplaceholder.typicode.com/comments
-
 //реалізувати UPDATE з Cars
 //додати кнопку DELETE коло Cars
 
 import React, {useEffect, useState} from 'react';
-import User from "../User/User";
-import {axiosService} from "../../services/axios.service";
 import {userService} from "../../services/user.service";
 import UserForm from "../UserForm/UserForm";
+import User from "../User/User";
 
 const Users = () => {
     const [users, setUsers] = useState([])
-    const [allUsers, setAllUsers] = useState(null)
     useEffect(() => {
         userService.getAll().then(value => value.data).then(value => setUsers(value));
-    }, [allUsers])
+    }, [])
     return (
         <div>
-            <UserForm setAllUsers={setAllUsers}/>
+            <UserForm users={users} setUsers={setUsers}/>
             <br/>
             {
                 users.map(user => <User user={user} key={user.id}/>)
