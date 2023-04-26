8afb85ba2ca0fd94e5bf876710e1563629d18807
YZahorodnii <yzagorodniy@gmail.com>
2023-04-26 19:47:32 +0100

    homework lesson 5 CLASS

--- src/App.js
+++ src/App.js
@@ -1,11 +1,5 @@
 // отримати пости та вивести їх використовуючи класові копмоненти
 // отримати коментарі та вивести їх використовуючи класові копмоненти
-//
-// Використовуючи хук useReducer в якому початковый стейт буде {cats: [], dogs:[]}
-// Реалізвути збереження окремо нового кота, та окремо собаку (приклад на зображенні)
-//
-// Advance
-// реалізувати видалення для кожного при натисканні на кнопку delete
 
 import {Posts} from "./components/Posts/Posts";
 import {Comments} from "./components/Comments/Comments";
@@ -13,8 +7,8 @@ import {Comments} from "./components/Comments/Comments";
 function App() {
   return (
     <div>
-      <Posts/>
-        {/*<Comments/>*/}
+      {/*<Posts/>*/}
+        <Comments/>
     </div>
   );
 }
