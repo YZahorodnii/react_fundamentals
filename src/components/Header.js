2c59703c26ca8aed8bfdc8e4f85ee4459c62e9c9
YZahorodnii <yzagorodniy@gmail.com>
2023-05-05 15:19:07 +0100

    practice typescript on lesson3 homework

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import {NavLink} from "react-router-dom";
+import css from './Header.module.css'
 
 const Header = () => {
     return (
-        <div>
+        <div className={css.Header}>
             <NavLink to={'/'}>Home</NavLink>
             <NavLink to={'/cars'}>Cars</NavLink>
         </div>
--- src/layouts/MainLayout.js
+++ src/layouts/MainLayout.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import Header from "../components/Header";
 import CarsPage from "../pages/CarsPage";
+import {Outlet} from "react-router-dom";
 
 const MainLayout = () => {
     return (
         <div>
             <Header/>
-
+            <Outlet/>
         </div>
     );
 };
