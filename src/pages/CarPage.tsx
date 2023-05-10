2f7f107b99c07dd73cb78f1a4757ad91b295399b
YZahorodnii <yzagorodniy@gmail.com>
2023-05-10 15:53:59 +0100

    Lesson 8 Redux Toolkit Typescript(TS)

--- src/pages/CarPage.tsx
+++ src/pages/CarPage.tsx
@@ -1,11 +1,17 @@
 import React, {FC} from 'react';
 import {CarForm} from "../components/CarForm";
 import {Cars} from "../components/Cars";
+import {useAppSelector} from "../hooks";
 
 const CarPage: FC = () => {
+    const {errors} = useAppSelector(state => state.carReducer)
     return (
         <div>
             <CarForm/>
+            {errors?.detail&&<h1>{errors.detail}</h1>}
+            {errors?.brand&&<h1>{errors.brand}</h1>}
+            {errors?.price&&<h1>{errors.price}</h1>}
+            {errors?.year&&<h1>{errors.year}</h1>}
             <Cars/>
         </div>
     );
