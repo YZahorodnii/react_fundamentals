8f0f067cda352018e8f942ea9c6f20a69297b1c2
YZahorodnii <yzagorodniy@gmail.com>
2023-05-11 12:15:23 +0100

    pagination practice

--- src/components/Characters.js
+++ src/components/Characters.js
@@ -3,13 +3,19 @@ import {useDispatch, useSelector} from "react-redux";
 import {Character} from "./Character";
 import {charactersService} from "../services";
 import {charactersActions} from "../redux/slices";
+import {useSearchParams} from "react-router-dom";
 
 const Characters = () => {
     const dispatch = useDispatch();
     const {characters} = useSelector(state => state.characters);
+    const [query, ] = useSearchParams({page: '1'});
     useEffect(() => {
-        charactersService.getAll().then(value => value.data).then(value => dispatch(charactersActions.setCharacters(value)))
-    }, [])
+
+    })
+
+    useEffect(() => {
+        charactersService.getAll(+query.get('page')).then(value => value.data).then(value => dispatch(charactersActions.setCharacters(value)))
+    }, [query])
     return (
         <div>
             {characters.map(character => <Character key={character.id} character={character}/>)}
